44006d7131a4f9b0ddd1bf467897b8bd2ad69d4d
Davide <d.sadotti@gmail.com>
2021-05-27 17:44:21 +0200

    problema con il form di aggiunta commenti

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Comment;
 use App\Post;
+use App\Tag;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
@@ -10,9 +13,10 @@ class BlogController extends Controller
 
         // PRENDO I DATI DAL DB
         $posts = Post::where('published', 1)->orderBy('date', 'asc')->get();
+        $tags = Tag::all();
 
         // RESTITUISCO LA PAGINA HOME
-        return view('guest.index', compact('posts'));
+        return view('guest.index', compact('posts', 'tags'));
 
     }
 
@@ -20,14 +24,44 @@ class BlogController extends Controller
 
         // PRENDO I DATI DAL DB
         $post = Post::where('slug', $slug)->first();
+        $tags = Tag::all();
 
         if($post == null){
             abort(404);
         }
 
         // RESTITUISCO LA PAGINA DEL POST
-        return view('guest.show', compact('post'));
+        return view('guest.show', compact('post', 'tags'));
 
+    }
+
+    public function filterTag($slug){
+
+        $tags = Tag::all();
+        $tag = Tag::where('slug', $slug)->first();
+        if($tag == null){
+            abort(404);
+        }
+        $posts = $tag->posts()->where('published', 1)->get();
+
+        return view('guest.index', compact('posts', 'tags'));
+    }
+
+    public function addComment(Request $request){
+
+        dd($request->all());
+        // $request->validate([
+        //     'name' => 'nullable|string|max:100',
+        //     'content' => 'required|string'
+        // ]);
+
+        // $newComment = new Comment();
+        // $newComment->name = $request->name;
+        // $newComment->content = $request->content;
+        // $newComment->post_id = $post->id;
+        // $newComment->save();
+
+        // return back();
     }
 
 }
--- resources/views/guest/show.blade.php
+++ resources/views/guest/show.blade.php
@@ -10,18 +10,34 @@
 	<h4>{{$post->date}}</h4>
 	<p>{{$post->content}}</p>
 
-	@if ($post->comments->isNotEmpty())
 	<div class="mt-5">
-		<h3>Commenti</h3>
-		<ul>
-			@foreach ($post->comments as $comment)
-				<li>
-					<h5>{{$comment->name ? $comment->name : 'Anonimo'}}</h5>
-					<p>{{$comment->content}}</p>
-				</li>
-			@endforeach
-		</ul>
+		@if ($post->comments->isNotEmpty())
+			<h3>Commenti</h3>
+			<ul>
+				@foreach ($post->comments as $comment)
+					<li>
+						<h5>{{$comment->name ? $comment->name : 'Anonimo'}}</h5>
+						<p>{{$comment->content}}</p>
+					</li>
+				@endforeach
+			</ul>
+		@endif
+
+		<form action="{{route('guest.posts.add-comment', ['post' => $post->id])}}" method="post">
+			@csrf
+			@method('POST')
+			<div class="form-group">
+				<label for="title">Nome</label>
+				<input type="text" class="form-control" id="name" name="name" placeholder="Nome">
+			</div>
+			<div class="form-group">
+				<label for="content">Commento</label>
+				<textarea class="form-control"  name="content" id="content" cols="30" rows="4" placeholder="Commento"></textarea>
+			</div>
+			<div class="mt-3">
+				<button type="submit" class="btn btn-primary">Inserisci</button>
+			</div>
+		</form>
 	</div>
-	@endif
 </div>
 @endsection
